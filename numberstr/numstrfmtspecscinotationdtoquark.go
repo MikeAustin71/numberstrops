506c8f25edcd750c1cf318f105ed9781206818a6
Mike Rapp <jordy.rascal@protonmail.com>
2021-01-22 23:45:57 -0600

    Added new method numStrFmtSpecSciNotationDtoQuark) testValidityOfNumStrFmtSpecSciNotationDto().

---
+++ numberstr/numstrfmtspecscinotationdtoquark.go
@@ -0,0 +1,89 @@
+package numberstr
+
+import (
+	"fmt"
+	"sync"
+)
+
+type numStrFmtSpecSciNotationDtoQuark struct {
+	lock *sync.Mutex
+}
+
+// testValidityOfNumStrFmtSpecSciNotationDto - Receives an instance
+// of NumStrFmtSpecSciNotationDto and proceeds to test the validity
+// of the member data fields. The NumStrFmtSpecSciNotationDto
+// object encapsulates scientific notation formatting for number
+// string text displays.
+//
+// If one or more data elements are found to be invalid, an
+// error is returned and the return boolean parameter, 'isValid',
+// is set to 'false'.
+//
+// If nStrFmtSpecSicNotDto.mantissaLength is greater than 1000,
+// this method will return an error.
+//
+//
+func (nStrFmtSpecSciNotQuark *numStrFmtSpecSciNotationDtoQuark) testValidityOfNumStrFmtSpecSciNotationDto(
+	nStrFmtSpecSicNotDto *NumStrFmtSpecSciNotationDto,
+	ePrefix string) (
+	isValid bool,
+	err error) {
+
+	if nStrFmtSpecSciNotQuark.lock == nil {
+		nStrFmtSpecSciNotQuark.lock = new(sync.Mutex)
+	}
+
+	nStrFmtSpecSciNotQuark.lock.Lock()
+
+	defer nStrFmtSpecSciNotQuark.lock.Unlock()
+
+	ePrefix += "numStrFmtSpecSciNotationDtoQuark.testValidityOfNumStrFmtSpecSciNotationDto()\n"
+
+	isValid = false
+
+	if nStrFmtSpecSicNotDto == nil {
+		err = fmt.Errorf("%v\n"+
+			"Error: Input parameter 'nStrFmtSpecSicNotDto' is invalid!\n"+
+			"'nStrFmtSpecSicNotDto' is a 'nil' pointer\n",
+			ePrefix)
+
+		return isValid, err
+	}
+
+	if nStrFmtSpecSicNotDto.lock == nil {
+		nStrFmtSpecSicNotDto.lock = new(sync.Mutex)
+	}
+
+	if nStrFmtSpecSicNotDto.mantissaLength > 1000 {
+		err = fmt.Errorf("%v\n"+
+			"Error: 'nStrFmtSpecSicNotDto.mantissaLength' is invalid!\n"+
+			"nStrFmtSpecSicNotDto.mantissaLength is greater than 1000.\n"+
+			"nStrFmtSpecSicNotDto.mantissaLength== '%v'\n",
+			ePrefix,
+			nStrFmtSpecSicNotDto.mantissaLength)
+
+		return isValid, err
+	}
+
+	if nStrFmtSpecSicNotDto.exponentChar == 0 {
+		err = fmt.Errorf("%v\n"+
+			"Error: 'nStrFmtSpecSicNotDto.exponentChar' is invalid!\n"+
+			"nStrFmtSpecSicNotDto.exponentChar== '%v'\n",
+			ePrefix,
+			nStrFmtSpecSicNotDto.exponentChar)
+
+		return isValid, err
+	}
+
+	err = nStrFmtSpecSicNotDto.numFieldLenDto.IsValidInstanceError(
+		ePrefix +
+			"\nTesting Validity of nStrFmtSpecSicNotDto.numFieldLenDto\n")
+
+	if err != nil {
+		return isValid, err
+	}
+
+	isValid = true
+
+	return isValid, err
+}
