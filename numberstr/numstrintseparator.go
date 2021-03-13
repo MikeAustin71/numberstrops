13351cbf6d20df8a538cf19875fc01b5e3583d12
Mike Rapp <jordy.rascal@protonmail.com>
2021-03-13 00:04:35 -0600

    Added new method NumStrIntSeparator.NewWithComponents()

--- numberstr/numstrintseparator.go
+++ numberstr/numstrintseparator.go
@@ -298,11 +298,94 @@ func (nStrIntSep *NumStrIntSeparator) IsValidInstanceError(
 	return err
 }
 
+// NewWithComponents - Creates and returns a new instance of
+// NumStrIntSeparator. The new instance is generated based on
+// component elements passed as input parameters.
+//
+//
+// ----------------------------------------------------------------
+//
+// Input Parameters
+//
+//  intSeparatorChars          []rune
+//     - A series of runes or characters used to separate integer
+//       digits in a number string. These characters are commonly
+//       known as the 'thousands separator'. A 'thousands
+//       separator' is used to separate groups of integer digits to
+//       the left of the decimal separator (a.k.a. decimal point).
+//       In the United States, the standard integer digits
+//       separator is the single comma character (','). Other
+//       countries and cultures use periods, spaces, apostrophes or
+//       multiple characters to separate integers.
+//             United States Example:  1,000,000,000
+//
+//
+//  intSeparatorGrouping       uint
+//     - This unsigned integer values specifies the number of
+//       integer digits within a group. This value is used to group
+//       integers within a number string.
+//
+//       In most western countries integer digits to the left of
+//       the decimal separator (a.k.a. decimal point) are separated
+//       into groups of three digits representing a grouping of
+//       'thousands' like this: '1,000,000,000'. In this case the
+//       intSeparatorGrouping value would be set to three ('3').
+//
+//       In some countries and cultures other integer groupings are
+//       used. In India, for example, a number might be formatted
+//       like this: '6,78,90,00,00,00,00,000'.
+//
+//
+//  intSeparatorRepetitions    uint
+//     - This unsigned integer value specifies the number of times
+//       this integer grouping is repeated. A value of zero signals
+//       that this integer grouping will be repeated indefinitely.
+//
+//
+//  restartIntGroupingSequence bool
+//     - If the NumStrIntSeparator is the last element in an array
+//       of NumStrIntSeparator objects, this boolean flag signals
+//       whether the entire integer grouping sequence will be
+//       restarted from array element zero.
+//
+//
+//  ePrefix                    *ErrPrefixDto
+//     - This object encapsulates an error prefix string which is
+//       included in all returned error messages. Usually, it
+//       contains the names of the calling method or methods.
+//
+//       If no error prefix information is needed, set this parameter
+//       to 'nil'.
+//
+//
+// -----------------------------------------------------------------
+//
+// Return Values
+//
+//  newIntSep                  NumStrIntSeparator
+//     - If this method completes successfully, the parameter will
+//       return a new and fully populated instance of
+//       NumStrIntSeparator.
+//
+//
+//  err                        error
+//     - If this method completes successfully, the returned error
+//       Type is set equal to 'nil'.
+//
+//       If errors are encountered during processing, the returned
+//       error Type will encapsulate an error message. This
+//       returned error message will incorporate the method chain
+//       and text passed by input parameter, 'ePrefix'. The
+//       'ePrefix' text will be attached to the beginning of the
+//       error message.
+//
 func (nStrIntSep NumStrIntSeparator) NewWithComponents(
 	intSeparatorChars []rune,
 	intSeparatorGrouping uint,
+	intSeparatorRepetitions uint,
 	restartIntGroupingSequence bool,
 	ePrefix *ErrPrefixDto) (
+	newIntSep NumStrIntSeparator,
 	err error) {
 
 	if nStrIntSep.lock == nil {
@@ -319,4 +402,15 @@ func (nStrIntSep NumStrIntSeparator) NewWithComponents(
 
 	ePrefix.SetEPref("NumStrIntSeparator.NewWithComponents()")
 
+	err =
+		numStrIntSeparatorMechanics{}.ptr().
+			setWithComponents(
+				&newIntSep,
+				intSeparatorChars,
+				intSeparatorGrouping,
+				intSeparatorRepetitions,
+				restartIntGroupingSequence,
+				ePrefix)
+
+	return newIntSep, err
 }
--- numberstr/numstrintseparatormechanics.go
+++ numberstr/numstrintseparatormechanics.go
@@ -28,6 +28,102 @@ func (intSeparatorMech numStrIntSeparatorMechanics) ptr() *numStrIntSeparatorMec
 	return newIntSeparatorMech
 }
 
+// setToUSADefaults - Receives a pointer to an instance of
+// NumStrIntSeparator and proceeds to overwrite and set the
+// internal member variable data values to default values
+// used in the United States. Integer separator values used
+// in the United States consist of the comma character (','),
+// an integer grouping of three ('3') and unlimited repetitions of
+// this sequence.
+//   United States Integer Separation Example:
+//         '1,000,000,000,000'
+//
+//
+// ----------------------------------------------------------------
+//
+// Input Parameters
+//
+//  nStrIntSep                 *NumStrIntSeparator
+//     - A pointer to an instance of NumStrIntSeparator. All the
+//       internal member variable data values contained in this
+//       object will be overwritten and reset to default integer
+//       separator values used in the United States.
+//           United States Integer Separation Example:
+//                  '1,000,000,000,000'
+//
+//
+//  ePrefix             *ErrPrefixDto
+//     - This object encapsulates an error prefix string which is
+//       included in all returned error messages. Usually, it
+//       contains the names of the calling method or methods.
+//
+//       If no error prefix information is needed, set this parameter
+//       to 'nil'.
+//
+//
+// -----------------------------------------------------------------
+//
+// Return Values
+//
+//  error
+//     - If this method completes successfully, the returned error
+//       Type is set equal to 'nil'.
+//
+//       If errors are encountered during processing, the returned
+//       error Type will encapsulate an error message. This
+//       returned error message will incorporate the method chain
+//       and text passed by input parameter, 'ePrefix'. The
+//       'ePrefix' text will be attached to the beginning of the
+//       error message.
+//
+func (intSeparatorMech *numStrIntSeparatorMechanics) setToUSADefaults(
+	nStrIntSep *NumStrIntSeparator,
+	ePrefix *ErrPrefixDto) (
+	err error) {
+
+	if intSeparatorMech.lock == nil {
+		intSeparatorMech.lock = new(sync.Mutex)
+	}
+
+	intSeparatorMech.lock.Lock()
+
+	defer intSeparatorMech.lock.Unlock()
+
+	if ePrefix == nil {
+		ePrefix = ErrPrefixDto{}.Ptr()
+	}
+
+	ePrefix.SetEPref(
+		"numStrIntSeparatorMechanics." +
+			"setToUSADefaults()")
+
+	if nStrIntSep == nil {
+		err = fmt.Errorf("%v\n"+
+			"Error: Input parameter 'nStrIntSep' is invalid!\n"+
+			"'nStrIntSep' is a nil pointer.",
+			ePrefix.String())
+
+		return err
+	}
+
+	if nStrIntSep.lock == nil {
+		nStrIntSep.lock = new(sync.Mutex)
+	}
+
+	nStrIntSep.intSeparatorChars =
+		make([]rune, 1, 2)
+
+	nStrIntSep.intSeparatorChars[0] = ','
+
+	nStrIntSep.intSeparatorGrouping = 3
+
+	nStrIntSep.intSeparatorRepetitions = 0
+
+	nStrIntSep.restartIntGroupingSequence = false
+
+	return err
+}
+
 // setWithComponents - Receives a pointer to an instance of
 // NumStrIntSeparator and proceeds to overwrite and set the
 // internal member variable data values based on the other input
@@ -44,6 +140,7 @@ func (intSeparatorMech numStrIntSeparatorMechanics) ptr() *numStrIntSeparatorMec
 //       object will be overwritten and reset based on the
 //       following input parameters.
 //
+//
 //  intSeparatorChars          []rune
 //     - A series of runes or characters used to separate integer
 //       digits in a number string. These characters are commonly
@@ -55,6 +152,7 @@ func (intSeparatorMech numStrIntSeparatorMechanics) ptr() *numStrIntSeparatorMec
 //       countries and cultures use periods, spaces, apostrophes or
 //       multiple characters to separate integers.
 //             United States Example:  1,000,000,000
+//
 //
 //  intSeparatorGrouping       uint
 //     - This unsigned integer values specifies the number of
@@ -70,6 +168,44 @@ func (intSeparatorMech numStrIntSeparatorMechanics) ptr() *numStrIntSeparatorMec
 //       In some countries and cultures other integer groupings are
 //       used. In India, for example, a number might be formatted
 //       like this: '6,78,90,00,00,00,00,000'.
+//
+//
+//  intSeparatorRepetitions    uint
+//     - This unsigned integer value specifies the number of times
+//       this integer grouping is repeated. A value of zero signals
+//       that this integer grouping will be repeated indefinitely.
+//
+//
+//  restartIntGroupingSequence bool
+//     - If the NumStrIntSeparator is the last element in an array
+//       of NumStrIntSeparator objects, this boolean flag signals
+//       whether the entire integer grouping sequence will be
+//       restarted from array element zero.
+//
+//
+//  ePrefix                    *ErrPrefixDto
+//     - This object encapsulates an error prefix string which is
+//       included in all returned error messages. Usually, it
+//       contains the names of the calling method or methods.
+//
+//       If no error prefix information is needed, set this parameter
+//       to 'nil'.
+//
+//
+// -----------------------------------------------------------------
+//
+// Return Values
+//
+//  error
+//     - If this method completes successfully, the returned error
+//       Type is set equal to 'nil'.
+//
+//       If errors are encountered during processing, the returned
+//       error Type will encapsulate an error message. This
+//       returned error message will incorporate the method chain
+//       and text passed by input parameter, 'ePrefix'. The
+//       'ePrefix' text will be attached to the beginning of the
+//       error message.
 //
 func (intSeparatorMech *numStrIntSeparatorMechanics) setWithComponents(
 	nStrIntSep *NumStrIntSeparator,
@@ -103,6 +239,10 @@ func (intSeparatorMech *numStrIntSeparatorMechanics) setWithComponents(
 			ePrefix.String())
 
 		return err
+	}
+
+	if nStrIntSep.lock == nil {
+		nStrIntSep.lock = new(sync.Mutex)
 	}
 
 	if intSeparatorChars == nil {
