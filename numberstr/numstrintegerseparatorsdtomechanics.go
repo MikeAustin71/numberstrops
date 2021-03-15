ce15eb63195b0969d1a81a8a7afd5e91f0c0c5f6
Mike Rapp <jordy.rascal@protonmail.com>
2021-03-15 00:17:05 -0500

    Refactored numStrIntSeparatorsDtoMechanics

--- numberstr/numstrintegerseparatorsdtomechanics.go
+++ numberstr/numstrintegerseparatorsdtomechanics.go
@@ -29,6 +29,62 @@ func (intSepsDtoMech numStrIntSeparatorsDtoMechanics) ptr() *numStrIntSeparators
 	return newMech
 }
 
+// setToUSADefaults - Sets the member variable data
+// values of the incoming NumStrIntSeparatorsDto instance to United
+// States default values.
+//
+func (intSepsDtoMech *numStrIntSeparatorsDtoMechanics) setToUSADefaults(
+	intSepsDto *NumStrIntSeparatorsDto,
+	ePrefix *ErrPrefixDto) (
+	err error) {
+
+	if intSepsDtoMech.lock == nil {
+		intSepsDtoMech.lock = new(sync.Mutex)
+	}
+
+	intSepsDtoMech.lock.Lock()
+
+	defer intSepsDtoMech.lock.Unlock()
+
+	if ePrefix == nil {
+		ePrefix = ErrPrefixDto{}.Ptr()
+	}
+
+	ePrefix.SetEPref(
+		"numStrIntSeparatorsDtoMechanics." +
+			"setToUSADefaults()")
+
+	if intSepsDto == nil {
+		err = fmt.Errorf("%v\n"+
+			"Error: Input parameter 'intSepsDto' is invalid!\n"+
+			"'intSepsDto' is a 'nil' pointer.\n",
+			ePrefix.String())
+
+		return err
+	}
+
+	if intSepsDto.lock == nil {
+		intSepsDto.lock = new(sync.Mutex)
+	}
+
+	intSepsDto.intSeparators =
+		make([]NumStrIntSeparator, 1, 5)
+
+	intSepsDto.intSeparators[0].intSeparatorChars =
+		make([]rune, 1, 5)
+
+	intSepsDto.intSeparators[0].intSeparatorChars[0] = ','
+
+	intSepsDto.intSeparators[0].intSeparatorGrouping = 3
+
+	intSepsDto.intSeparators[0].intSeparatorRepetitions = 0
+
+	intSepsDto.intSeparators[0].restartIntGroupingSequence =
+		false
+
+	return err
+}
+
 // setWithComponents - Sets the member variable data values of the
 // incoming NumStrIntSeparatorsDto instance with the array values
 // passed by input parameter 'integerSeparatorsDto'.
@@ -100,62 +156,6 @@ func (intSepsDtoMech *numStrIntSeparatorsDtoMechanics) setWithComponents(
 			return err
 		}
 	}
-
-	return err
-}
-
-// setToUSADefaults - Sets the member variable data
-// values of the incoming NumStrIntSeparatorsDto instance to United
-// States default values.
-//
-func (intSepsDtoMech *numStrIntSeparatorsDtoMechanics) setToUSADefaults(
-	intSepsDto *NumStrIntSeparatorsDto,
-	ePrefix *ErrPrefixDto) (
-	err error) {
-
-	if intSepsDtoMech.lock == nil {
-		intSepsDtoMech.lock = new(sync.Mutex)
-	}
-
-	intSepsDtoMech.lock.Lock()
-
-	defer intSepsDtoMech.lock.Unlock()
-
-	if ePrefix == nil {
-		ePrefix = ErrPrefixDto{}.Ptr()
-	}
-
-	ePrefix.SetEPref(
-		"numStrIntSeparatorsDtoMechanics." +
-			"setToUSADefaults()")
-
-	if intSepsDto == nil {
-		err = fmt.Errorf("%v\n"+
-			"Error: Input parameter 'intSepsDto' is invalid!\n"+
-			"'intSepsDto' is a 'nil' pointer.\n",
-			ePrefix.String())
-
-		return err
-	}
-
-	if intSepsDto.lock == nil {
-		intSepsDto.lock = new(sync.Mutex)
-	}
-
-	intSepsDto.intSeparators =
-		make([]NumStrIntSeparator, 1, 5)
-
-	intSepsDto.intSeparators[0].intSeparatorChars =
-		make([]rune, 1, 5)
-
-	intSepsDto.intSeparators[0].intSeparatorChars[0] = ','
-
-	intSepsDto.intSeparators[0].intSeparatorGrouping = 3
-
-	intSepsDto.intSeparators[0].intSeparatorRepetitions = 0
-
-	intSepsDto.intSeparators[0].restartIntGroupingSequence =
-		false
 
 	return err
 }
