5505f6069fa2dd24e3d48b0b114c686a83424f4b
Mike Rapp <jordy.rascal@protonmail.com>
2021-03-21 14:56:54 -0500

    Purged old tests and structures in preparation for complete re-write.

--- numberstr/numstrfmtspecdtoelectron.go
+++ numberstr/numstrfmtspecdtoelectron.go
@@ -1,7 +1,6 @@
 package numberstr
 
 import (
-	"fmt"
 	"sync"
 )
 
@@ -9,217 +8,22 @@ type numStrFmtSpecDtoElectron struct {
 	lock *sync.Mutex
 }
 
-// copyIn - Copies the data fields from input parameter
-// 'inComingNStrFmtSpecDto' to input parameter
-// 'targetNStrFmtSpecDto'.
+// ptr - Returns a pointer to a new instance of
+// numStrFmtSpecDtoElectron.
 //
-// Be advised - All data fields in 'targetNStrFmtSpecDto'
-// will be overwritten.
-//
-// If input parameter 'inComingNStrFmtSpecDto' is judged
-// to be invalid, this method will return an error.
-//
-func (nStrFmtSpecDtoElectron *numStrFmtSpecDtoElectron) copyIn(
-	targetNStrFmtSpecDto *NumStrFmtSpecDto,
-	inComingNStrFmtSpecDto *NumStrFmtSpecDto,
-	ePrefix *ErrPrefixDto) (
-	err error) {
+func (nStrFmtSpecElectron numStrFmtSpecDtoElectron) ptr() *numStrFmtSpecDtoElectron {
 
-	if nStrFmtSpecDtoElectron.lock == nil {
-		nStrFmtSpecDtoElectron.lock = new(sync.Mutex)
+	if nStrFmtSpecElectron.lock == nil {
+		nStrFmtSpecElectron.lock = new(sync.Mutex)
 	}
 
-	nStrFmtSpecDtoElectron.lock.Lock()
+	nStrFmtSpecElectron.lock.Lock()
 
-	defer nStrFmtSpecDtoElectron.lock.Unlock()
+	defer nStrFmtSpecElectron.lock.Unlock()
 
-	if ePrefix == nil {
-		ePrefix = ErrPrefixDto{}.Ptr()
-	}
+	newNumStrFmtSpecElectron := new(numStrFmtSpecDtoElectron)
 
-	ePrefix.SetEPref("numStrFmtSpecDtoElectron.copyIn()")
+	newNumStrFmtSpecElectron.lock = new(sync.Mutex)
 
-	if targetNStrFmtSpecDto == nil {
-		err = fmt.Errorf("%v\n"+
-			"Error: Input parameter 'targetNStrFmtSpecDto' is"+
-			" a 'nil' pointer!\n",
-			ePrefix.String())
-		return err
-	}
-
-	if targetNStrFmtSpecDto.lock == nil {
-		targetNStrFmtSpecDto.lock = new(sync.Mutex)
-	}
-
-	if inComingNStrFmtSpecDto == nil {
-		err = fmt.Errorf("%v\n"+
-			"Error: Input parameter 'inComingNStrFmtSpecDto' is"+
-			" a 'nil' pointer!\n",
-			ePrefix.String())
-		return err
-	}
-
-	nStrFmtSpecDtoQuark :=
-		numStrFmtSpecDtoQuark{}
-
-	_,
-		err =
-		nStrFmtSpecDtoQuark.testValidityOfNumStrFmtSpecDto(
-			inComingNStrFmtSpecDto,
-			ePrefix.XCtx("Testing validity of 'inComingNStrFmtSpecDto'"))
-
-	if err != nil {
-		return err
-	}
-
-	targetNStrFmtSpecDto.idNo =
-		inComingNStrFmtSpecDto.idNo
-
-	targetNStrFmtSpecDto.idString =
-		inComingNStrFmtSpecDto.idString
-
-	targetNStrFmtSpecDto.description =
-		inComingNStrFmtSpecDto.description
-
-	targetNStrFmtSpecDto.tag =
-		inComingNStrFmtSpecDto.tag
-
-	err = targetNStrFmtSpecDto.countryCulture.CopyIn(
-		&inComingNStrFmtSpecDto.countryCulture,
-		ePrefix.XCtx(
-			"inComingNStrFmtSpecDto->targetNStrFmtSpecDto"))
-
-	if err != nil {
-		return err
-	}
-
-	err = targetNStrFmtSpecDto.absoluteValue.CopyIn(
-		&inComingNStrFmtSpecDto.absoluteValue,
-		ePrefix.XCtx("inComingNStrFmtSpecDto->targetNStrFmtSpecDto"))
-
-	if err != nil {
-		return err
-	}
-
-	err = targetNStrFmtSpecDto.currencyValue.CopyIn(
-		&inComingNStrFmtSpecDto.currencyValue,
-		ePrefix.XCtx("inComingNStrFmtSpecDto->targetNStrFmtSpecDto"))
-
-	if err != nil {
-		return err
-	}
-
-	err = targetNStrFmtSpecDto.signedNumValue.CopyIn(
-		&inComingNStrFmtSpecDto.signedNumValue,
-		ePrefix.XCtx("inComingNStrFmtSpecDto->targetNStrFmtSpecDto"))
-
-	if err != nil {
-		return err
-	}
-
-	err = targetNStrFmtSpecDto.sciNotation.CopyIn(
-		&inComingNStrFmtSpecDto.sciNotation,
-		ePrefix.XCtx("inComingNStrFmtSpecDto->targetNStrFmtSpecDto"))
-
-	return err
-}
-
-// copyOut - Returns a deep copy of input parameter 'nStrFmtSpecDto'
-// styled as a new instance of NumStrFmtSpecDto.
-//
-// If input parameter 'nStrFmtSpecDto' is judged to be
-// invalid, this method will return an error.
-//
-func (nStrFmtSpecDtoElectron *numStrFmtSpecDtoElectron) copyOut(
-	nStrFmtSpecDto *NumStrFmtSpecDto,
-	ePrefix *ErrPrefixDto) (
-	newNStrFmtSpecDto NumStrFmtSpecDto,
-	err error) {
-
-	if nStrFmtSpecDtoElectron.lock == nil {
-		nStrFmtSpecDtoElectron.lock = new(sync.Mutex)
-	}
-
-	nStrFmtSpecDtoElectron.lock.Lock()
-
-	defer nStrFmtSpecDtoElectron.lock.Unlock()
-
-	if ePrefix == nil {
-		ePrefix = ErrPrefixDto{}.Ptr()
-	}
-
-	ePrefix.SetEPref(
-		"numStrFmtSpecDtoElectron.copyOut()")
-
-	if nStrFmtSpecDto == nil {
-		err = fmt.Errorf("%v\n"+
-			"Error: Input parameter 'nStrFmtSpecDto' is"+
-			" a 'nil' pointer!\n",
-			ePrefix.String())
-
-		return newNStrFmtSpecDto, err
-	}
-
-	nStrFmtSpecDtoQuark :=
-		numStrFmtSpecDtoQuark{}
-
-	_,
-		err =
-		nStrFmtSpecDtoQuark.testValidityOfNumStrFmtSpecDto(
-			nStrFmtSpecDto,
-			ePrefix.XCtx("Testing validity of 'nStrFmtSpecDto'"))
-
-	if err != nil {
-		return newNStrFmtSpecDto, err
-	}
-
-	newNStrFmtSpecDto.idNo =
-		nStrFmtSpecDto.idNo
-
-	newNStrFmtSpecDto.idString =
-		nStrFmtSpecDto.idString
-
-	newNStrFmtSpecDto.description =
-		nStrFmtSpecDto.description
-
-	newNStrFmtSpecDto.tag =
-		nStrFmtSpecDto.tag
-
-	err = newNStrFmtSpecDto.countryCulture.CopyIn(
-		&nStrFmtSpecDto.countryCulture,
-		ePrefix.XCtx("nStrFmtSpecDto->newNStrFmtSpecDto\n"))
-
-	if err != nil {
-		return newNStrFmtSpecDto, err
-	}
-
-	err = newNStrFmtSpecDto.absoluteValue.CopyIn(
-		&nStrFmtSpecDto.absoluteValue,
-		ePrefix.XCtx("nStrFmtSpecDto->newNStrFmtSpecDto"))
-
-	if err != nil {
-		return newNStrFmtSpecDto, err
-	}
-
-	err = newNStrFmtSpecDto.currencyValue.CopyIn(
-		&nStrFmtSpecDto.currencyValue,
-		ePrefix.XCtx("nStrFmtSpecDto->newNStrFmtSpecDto"))
-
-	if err != nil {
-		return newNStrFmtSpecDto, err
-	}
-
-	err = newNStrFmtSpecDto.signedNumValue.CopyIn(
-		&nStrFmtSpecDto.signedNumValue,
-		ePrefix.XCtx("nStrFmtSpecDto->newNStrFmtSpecDto"))
-
-	if err != nil {
-		return newNStrFmtSpecDto, err
-	}
-
-	err = newNStrFmtSpecDto.sciNotation.CopyIn(
-		&nStrFmtSpecDto.sciNotation,
-		ePrefix.XCtx("nStrFmtSpecDto->newNStrFmtSpecDto"))
-
-	return newNStrFmtSpecDto, err
+	return newNumStrFmtSpecElectron
 }
--- numberstr/numstrfmtspecdtoquark.go
+++ numberstr/numstrfmtspecdtoquark.go
@@ -1,7 +1,6 @@
 package numberstr
 
 import (
-	"fmt"
 	"sync"
 )
 
@@ -26,91 +25,4 @@ func (nStrFmtSpecDtoQuark numStrFmtSpecDtoQuark) ptr() *numStrFmtSpecDtoQuark {
 	newNumStrFmtSpecDtoQuark.lock = new(sync.Mutex)
 
 	return newNumStrFmtSpecDtoQuark
-}
-
-// testValidityOfNumStrFmtSpecDto - Receives an instance of
-// NumStrFmtSpecDto and proceeds to test the validity of the
-// member data fields.
-//
-// If one or more data elements are found to be invalid, an error
-// is returned and the return boolean parameter, 'isValid', is set
-// to 'false'.
-//
-func (nStrFmtSpecDtoQuark *numStrFmtSpecDtoQuark) testValidityOfNumStrFmtSpecDto(
-	nStrFmtSpecDto *NumStrFmtSpecDto,
-	ePrefix *ErrPrefixDto) (
-	isValid bool,
-	err error) {
-
-	if nStrFmtSpecDtoQuark.lock == nil {
-		nStrFmtSpecDtoQuark.lock = new(sync.Mutex)
-	}
-
-	nStrFmtSpecDtoQuark.lock.Lock()
-
-	defer nStrFmtSpecDtoQuark.lock.Unlock()
-
-	if ePrefix == nil {
-		ePrefix = ErrPrefixDto{}.Ptr()
-	}
-
-	ePrefix.SetEPref(
-		"numStrFmtSpecDtoQuark.testValidityOfNumStrFmtSpecDto()")
-
-	isValid = false
-
-	if nStrFmtSpecDto == nil {
-		err = fmt.Errorf("%v\n"+
-			"Error: Input parameter 'nStrFmtSpecDto' is nil pointer!\n",
-			ePrefix.String())
-
-		return isValid, err
-	}
-
-	if nStrFmtSpecDto.lock == nil {
-		nStrFmtSpecDto.lock = new(sync.Mutex)
-	}
-
-	err = nStrFmtSpecDto.countryCulture.IsValidInstanceError(
-		ePrefix.XCtx(
-			"Testing validity of " +
-				"nStrFmtSpecDto.countryCulture"))
-
-	if err != nil {
-		return isValid, err
-	}
-
-	err = nStrFmtSpecDto.absoluteValue.IsValidInstanceError(
-		ePrefix.XCtx(
-			"Testing validity of nStrFmtSpecDto" +
-				".absoluteValue "))
-
-	if err != nil {
-		return isValid, err
-	}
-
-	err = nStrFmtSpecDto.currencyValue.IsValidInstanceError(
-		ePrefix.XCtx("Testing validity of nStrFmtSpecDto.currencyValue"))
-
-	if err != nil {
-		return isValid, err
-	}
-
-	err = nStrFmtSpecDto.signedNumValue.IsValidInstanceError(
-		ePrefix.XCtx("Testing validity of nStrFmtSpecDto.signedNumValue"))
-
-	if err != nil {
-		return isValid, err
-	}
-
-	err = nStrFmtSpecDto.sciNotation.IsValidInstanceError(
-		ePrefix.XCtx("Testing validity of nStrFmtSpecDto.sciNotation"))
-
-	if err != nil {
-		return isValid, err
-	}
-
-	isValid = true
-
-	return isValid, err
 }
