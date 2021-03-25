f36f6d9aba4497adab161a9cd0ca4aef0b54001a
Mike Rapp <jordy.rascal@protonmail.com>
2021-03-24 22:52:39 -0500

    Added method FormatterOctal.NewWithComponents().

--- numberstr/formatteroctal.go
+++ numberstr/formatteroctal.go
@@ -781,8 +781,6 @@ func (fmtOctal FormatterOctal) NewWithComponents(
 		"FormatterOctal." +
 			"NewWithComponents()")
 
-	newFormatterOctal.lock = new(sync.Mutex)
-
 	err = formatterOctalMechanics{}.ptr().
 		setFmtOctalWithComponents(
 			&newFormatterOctal,
@@ -1205,4 +1203,228 @@ func (fmtOctal *FormatterOctal) SetTurnOnInterDigitsSeparation(
 		turnOnIntegerDigitsSeparation
 
 	return
+}
+
+// SetWithComponents - Overwrites and resets all the data values in
+// the current instance of FormatterOctal.
+//
+// Data values used to replace those in the current FormatterOctal
+// instance will be generated from components passed as input
+// parameters and detailed below.
+//
+//
+// ----------------------------------------------------------------
+//
+// Input Parameters
+//
+//  leftPrefix                    string
+//     - Sets the 'Left Prefix' string for the new returned
+//       instance of FormatterOctal.
+//
+//       'Left Prefix' string characters are appended or
+//        concatenated to the beginning of octal digits formatted
+//        in a text number string.
+//
+//       Occasionally, octal numeric digits displayed in a text
+//       string are prefixed with leading characters such as the
+//       digit zero ('0'), the letters 'o' or 'q', the digit–letter
+//       combination zero-'o' ('0o'), the ampersand symbol ('&'),
+//       the dollar sign symbol ('$'), the At Sign ('@') or the
+//       back slash symbol ('\').
+//
+//       The 'Left Prefix' parameter allows for custom
+//       configuration of these leading prefix characters.
+//
+//       An empty 'leftPrefix' string signals that no characters
+//       will be prefixed to the beginning of octal number strings.
+//
+//       Reference:
+//          https://en.wikipedia.org/wiki/Octal
+//
+//
+//  rightSuffix                   string
+//     - Sets the 'Right Suffix' string for the new returned
+//       instance of FormatterOctal. 'rightSuffix' specifies the
+//       characters which will be appended to the end of octal
+//       number strings.
+//
+//       Occasionally, octal numeric digits displayed in a text
+//       string are suffixed with trailing characters such as the
+//       the upper and lower case letters ('o','O', 'q' or 'Q').
+//
+//       The 'Right Suffix' parameter allows for custom
+//       configuration of these trailing suffix characters.
+//
+//       An empty 'rightSuffix' string signals that no characters
+//       will be appended to the end of octal number strings.
+//
+// Reference:
+//    https://en.wikipedia.org/wiki/Octal
+//
+//
+//  turnOnIntegerDigitsSeparation bool
+//     - Sets the 'Turn On Integer Digits Separation" flag.
+//          FormatterHexadecimal.turnOnIntegerDigitsSeparation
+//
+//       This boolean flag determines whether integer separation is
+//       applied to formatted hexadecimal digits in a number
+//       string.
+//
+//       If set to 'true', this flag signals that hexadecimal
+//       digits will be grouped and separated according to the
+//       specification contained in the internal member variable,
+//       'integerSeparators'. When set to 'false', this flag
+//       ensures that no integer digit separation will occur in
+//       formatted hexadecimal number strings.
+//
+//
+//  integerSeparators             NumStrIntSeparatorsDto
+//     - The NumStrIntSeparatorsDto type manages an internal
+//       Taken as a whole, these NumStrIntSeparator objects define
+//       the integer separation operation used in formatting number strings.
+//
+//        type NumStrIntSeparator struct {
+//            intSeparatorChars       []rune  // A series of runes used to separate integer digits.
+//            intSeparatorGrouping    uint    // Number of integer digits in a group
+//            intSeparatorRepetitions uint    // Number of times this character/group sequence is repeated
+//                                            // A zero value signals unlimited repetitions.
+//            restartIntGroupingSequence bool // If true, the entire grouping sequence is repeated
+//                                            //  beginning at array index zero.
+//        }
+//
+//        intSeparatorChars          []rune
+//           - A series of runes or characters used to separate integer
+//             digits in a number string. These characters are commonly
+//             known as the 'thousands separator'. A 'thousands
+//             separator' is used to separate groups of integer digits to
+//             the left of the decimal separator (a.k.a. decimal point).
+//             In the United States, the standard integer digits
+//             separator is the single comma character (','). Other
+//             countries and cultures use periods, spaces, apostrophes or
+//             multiple characters to separate integers.
+//                   United States Example:  1,000,000,000
+//
+//        intSeparatorGrouping       uint
+//           - This unsigned integer values specifies the number of
+//             integer digits within a group. This value is used to group
+//             integers within a number string.
+//
+//             In most western countries integer digits to the left of
+//             the decimal separator (a.k.a. decimal point) are separated
+//             into groups of three digits representing a grouping of
+//             'thousands' like this: '1,000,000,000'. In this case the
+//             intSeparatorGrouping value would be set to three ('3').
+//
+//             In some countries and cultures other integer groupings are
+//             used. In India, for example, a number might be formatted like
+//             this: '6,78,90,00,00,00,00,000'. Chinese Numerals have an
+//             integer grouping value of four ('4').
+//                Chinese Numerals Example: '12,3456,7890,2345'
+//
+//        intSeparatorRepetitions    uint
+//           - This unsigned integer value specifies the number of times
+//             this integer grouping is repeated. A value of zero signals
+//             that this integer grouping will be repeated indefinitely.
+//
+//        restartIntGroupingSequence bool
+//           - If the NumStrIntSeparator is the last element in an array
+//             of NumStrIntSeparator objects, this boolean flag signals
+//             whether the entire integer grouping sequence will be
+//             restarted from array element zero.
+//
+//
+//  numFieldDto                   NumberFieldDto
+//     - The NumberFieldDto object contains formatting instructions
+//       for the creation and implementation of a number field.
+//       Number fields are text strings which contain number strings
+//       for use in text displays.
+//
+//       The NumberFieldDto object contains specifications for number
+//       field length. Typically, the length of a number field is
+//       greater than the length of the number string which will be
+//       displayed within the number field.
+//
+//       The NumberFieldDto object also contains specifications
+//       for positioning or alignment of the number string within
+//       the number field. This alignment dynamic is described as
+//       text justification. The member variable '
+//       NumberFieldDto.textJustifyFormat' is used to specify one
+//       of three possible alignment formats. One of these formats
+//       will be selected to control the alignment of the number
+//       string within the number field. These optional alignment
+//       formats are shown below with examples:
+//
+//       (1) 'Right-Justification' - "       NumberString"
+//       (2) 'Left-Justification' - "NumberString        "
+//       (3) 'Centered'           - "    NumberString    "
+//
+//       The NumberFieldDto type is detailed as follows:
+//
+//       type NumberFieldDto struct {
+//         requestedNumFieldLength int // User requested number field length
+//         actualNumFieldLength    int // Machine generated actual number field Length
+//         minimumNumFieldLength   int // Machine generated minimum number field length
+//         textJustifyFormat       TextJustify // User specified text justification
+//       }
+//
+//
+//  ePrefix                       *ErrPrefixDto
+//     - This object encapsulates an error prefix string which is
+//       included in all returned error messages. Usually, it
+//       contains the names of the calling method or methods.
+//
+//       If no error prefix information is needed, set this parameter
+//       to 'nil'.
+//
+//
+// -----------------------------------------------------------------
+//
+// Return Values
+//
+//  err                           error
+//     - If this method completes successfully, the returned error
+//       Type is set equal to 'nil'.
+//
+//       If errors are encountered during processing, the returned
+//       error Type will encapsulate an error message. This
+//       returned error message will incorporate the method chain
+//       and text passed by input parameter, 'ePrefix'. The
+//       'ePrefix' text will be attached to the beginning of the
+//       error message.
+//
+func (fmtOctal *FormatterOctal) SetWithComponents(
+	leftPrefix string,
+	rightSuffix string,
+	turnOnIntegerDigitsSeparation bool,
+	integerSeparators NumStrIntSeparatorsDto,
+	numFieldDto NumberFieldDto,
+	ePrefix *ErrPrefixDto) error {
+
+	if fmtOctal.lock == nil {
+		fmtOctal.lock = new(sync.Mutex)
+	}
+
+	fmtOctal.lock.Lock()
+
+	defer fmtOctal.lock.Unlock()
+
+	if ePrefix == nil {
+		ePrefix = ErrPrefixDto{}.Ptr()
+	}
+
+	ePrefix.SetEPref(
+		"FormatterOctal." +
+			"SetWithComponents()")
+
+	return formatterOctalMechanics{}.ptr().
+		setFmtOctalWithComponents(
+			fmtOctal,
+			leftPrefix,
+			rightSuffix,
+			turnOnIntegerDigitsSeparation,
+			integerSeparators,
+			numFieldDto,
+			ePrefix.XCtx(
+				"newFormatterOctal"))
+
 }
