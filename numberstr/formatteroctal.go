6615a0a82a1391e83cdb85ff18f6fd80bd09c3ec
Mike Rapp <jordy.rascal@protonmail.com>
2021-04-03 00:29:47 -0500

    Added new method FormatterOctal.Equal()

--- numberstr/formatteroctal.go
+++ numberstr/formatteroctal.go
@@ -600,6 +600,87 @@ func (fmtOctal *FormatterOctal) Empty() {
 
 }
 
+// Equal - Receives a FormatterOctal object ('fmtOctalTwo')
+// and proceeds to determine whether all data elements in this
+// object are equal to all corresponding data elements in the
+// current instance of FormatterOctal.
+//
+//
+// ----------------------------------------------------------------
+//
+// Input Parameters
+//
+//  fmtOctalTwo      FormatterOctal
+//     - This method will compare all data elements in the current
+//       FormatterOctal instance to all corresponding data
+//       elements in this second FormatterOctal object to
+//       determine if they are equivalent.
+//
+//
+//  ePrefix             *ErrPrefixDto
+//     - This object encapsulates an error prefix string which is
+//       included in all returned error messages. Usually, it
+//       contains the names of the calling method or methods.
+//
+//       If no error prefix information is needed, set this
+//       parameter to 'nil'.
+//
+//
+// ------------------------------------------------------------------------
+//
+// Return Values
+//
+//  bool
+//     - If all the data elements in the current FormatterOctal
+//       instance are equal to all corresponding data elements in
+//       'fmtOctalTwo', this return parameter will be set to
+//       'true'. If all the data elements are NOT equal, this
+//       return parameter will be set to 'false'.
+//
+//
+//  error
+//     - If all the data elements in the current FormatterOctal
+//       instance are equal to all corresponding data elements in
+//       'fmtOctalTwo', this return parameter will be set to
+//       'nil'.
+//
+//       If the corresponding data elements are NOT equal, a
+//       detailed error message identifying the unequal elements
+//       will be returned.
+//
+func (fmtOctal *FormatterOctal) Equal(
+	fmtOctalTwo FormatterOctal,
+	ePrefix *ErrPrefixDto) (
+	bool,
+	error) {
+
+	if fmtOctal.lock == nil {
+		fmtOctal.lock = new(sync.Mutex)
+	}
+
+	fmtOctal.lock.Lock()
+
+	defer fmtOctal.lock.Unlock()
+
+	if ePrefix == nil {
+		ePrefix = ErrPrefixDto{}.Ptr()
+	} else {
+		ePrefix = ePrefix.CopyPtr()
+	}
+
+	ePrefix.SetEPref(
+		"FormatterOctal." +
+			"Equal()")
+
+	return formatterOctalAtom{}.ptr().
+		equal(
+			fmtOctal,
+			&fmtOctalTwo,
+			ePrefix.XCtx(
+				"fmtOctal vs "+
+					"fmtOctalTwo"))
+}
+
 // GetFmtNumStr - Returns a number string formatted for octal
 // digits based on the configuration encapsulated in the current
 // instance of FormatterOctal.
--- numberstr/formatteroctalatom.go
+++ numberstr/formatteroctalatom.go
@@ -195,6 +195,186 @@ func (fmtOctalAtom *formatterOctalAtom) copyOut(
 	return newFormatterOctal, err
 }
 
+// equal - Receives two FormatterOctal objects and proceeds to
+// determine whether all data elements in the first object are
+// equal to all corresponding data elements in the second object.
+//
+//
+// ----------------------------------------------------------------
+//
+// Input Parameters
+//
+//  fmtOctalOne         *FormatterOctal
+//     - A pointer to the first FormatterOctal object. This
+//       method will compare all data elements in this object to
+//       all corresponding data elements in the second
+//       FormatterOctal object to determine if they are equivalent.
+//
+//
+//  fmtOctalTwo         *FormatterOctal
+//     - A pointer to the second FormatterOctal object. This method
+//       will compare all data elements in the first FormatterOctal
+//       object to all corresponding data elements in this second
+//       FormatterOctal object to determine if they are equivalent.
+//
+//
+//  ePrefix             *ErrPrefixDto
+//     - This object encapsulates an error prefix string which is
+//       included in all returned error messages. Usually, it
+//       contains the names of the calling method or methods.
+//
+//       If no error prefix information is needed, set this parameter
+//       to 'nil'.
+//
+//
+// ------------------------------------------------------------------------
+//
+// Return Values
+//
+//  isEqual             bool
+//     - If all the data elements in 'fmtOctalOne' are equal to
+//       all the corresponding data elements in 'fmtOctalTwo',
+//       this return parameter will be set to 'true'. If the data
+//       data elements are NOT equal, this return parameter will be
+//       set to 'false'.
+//
+//
+//  err                 error
+//     - If all the data elements in 'fmtOctalOne' are equal to
+//       all the corresponding data elements in 'fmtOctalTwo',
+//       this return parameter will be set to 'nil'.
+//
+//       If the corresponding data elements are NOT equal, a
+//       detailed error message identifying the unequal elements
+//       will be returned.
+//
+func (fmtOctalAtom *formatterOctalAtom) equal(
+	fmtOctalOne *FormatterOctal,
+	fmtOctalTwo *FormatterOctal,
+	ePrefix *ErrPrefixDto) (
+	isEqual bool,
+	err error) {
+
+	if fmtOctalAtom.lock == nil {
+		fmtOctalAtom.lock = new(sync.Mutex)
+	}
+
+	fmtOctalAtom.lock.Lock()
+
+	defer fmtOctalAtom.lock.Unlock()
+
+	if ePrefix == nil {
+		ePrefix = ErrPrefixDto{}.Ptr()
+	} else {
+		ePrefix = ePrefix.CopyPtr()
+	}
+
+	ePrefix.SetEPref(
+		"formatterOctalAtom." +
+			"equal()")
+
+	isEqual = false
+
+	if fmtOctalOne == nil {
+
+		err = fmt.Errorf("%v\n"+
+			"Error: Input parameter 'fmtOctalOne' is a 'nil' pointer!\n",
+			ePrefix.String())
+
+		return isEqual, err
+	}
+
+	if fmtOctalTwo == nil {
+
+		err = fmt.Errorf("%v\n"+
+			"Error: Input parameter 'fmtOctalTwo' is a 'nil' pointer!\n",
+			ePrefix.String())
+
+		return isEqual, err
+	}
+
+	if fmtOctalOne.numStrFmtType !=
+		fmtOctalTwo.numStrFmtType {
+
+		err = fmt.Errorf("%v\n"+
+			"fmtOctalOne.numStrFmtType!=fmtOctalTwo.numStrFmtType\n"+
+			"fmtOctalOne.numStrFmtType='%v'\n"+
+			"fmtOctalTwo.numStrFmtType='%v'\n",
+			ePrefix.String(),
+			fmtOctalOne.numStrFmtType.String(),
+			fmtOctalTwo.numStrFmtType.String())
+
+		return isEqual, err
+	}
+
+	if fmtOctalOne.leftPrefix !=
+		fmtOctalTwo.leftPrefix {
+
+		err = fmt.Errorf("%v\n"+
+			"fmtOctalOne.leftPrefix!=fmtOctalTwo.leftPrefix\n"+
+			"fmtOctalOne.leftPrefix='%v'\n"+
+			"fmtOctalTwo.leftPrefix='%v'\n",
+			ePrefix.String(),
+			fmtOctalOne.leftPrefix,
+			fmtOctalTwo.leftPrefix)
+
+		return isEqual, err
+	}
+
+	if fmtOctalOne.rightSuffix !=
+		fmtOctalTwo.rightSuffix {
+
+		err = fmt.Errorf("%v\n"+
+			"fmtOctalOne.rightSuffix!=fmtOctalTwo.rightSuffix\n"+
+			"fmtOctalOne.rightSuffix='%v'\n"+
+			"fmtOctalTwo.rightSuffix='%v'\n",
+			ePrefix.String(),
+			fmtOctalOne.rightSuffix,
+			fmtOctalTwo.rightSuffix)
+
+		return isEqual, err
+	}
+
+	if fmtOctalOne.turnOnIntegerDigitsSeparation !=
+		fmtOctalTwo.turnOnIntegerDigitsSeparation {
+
+		err = fmt.Errorf("%v\n"+
+			"fmtOctalOne.turnOnIntegerDigitsSeparation!=\n"+
+			"  fmtOctalTwo.turnOnIntegerDigitsSeparation\n"+
+			"fmtOctalOne.turnOnIntegerDigitsSeparation='%v'\n"+
+			"fmtOctalTwo.turnOnIntegerDigitsSeparation='%v'\n",
+			ePrefix.String(),
+			fmtOctalOne.turnOnIntegerDigitsSeparation,
+			fmtOctalTwo.turnOnIntegerDigitsSeparation)
+
+		return isEqual, err
+	}
+
+	_,
+		err = fmtOctalOne.numericSeparators.Equal(
+		fmtOctalTwo.numericSeparators,
+		ePrefix.XCtx(
+			"fmtOctalOne vs fmtOctalTwo"))
+
+	if err != nil {
+		return isEqual, err
+	}
+
+	_,
+		err = fmtOctalOne.numFieldDto.Equal(
+		fmtOctalTwo.numFieldDto,
+		ePrefix.XCtx(
+			"fmtOctalOne vs fmtOctalTwo"))
+
+	if err != nil {
+		return isEqual, err
+	}
+
+	isEqual = true
+
+	return isEqual, err
+}
+
 // ptr - Returns a pointer to a new instance of
 // formatterOctalAtom.
 //
